86b5e9a7d43f9d5d746dc1f8ad3b49036b7f19a4
9515587792 <41770851+9515587792@users.noreply.github.com>
2018-09-24 10:43:11 +0530

    Create prg51.py

---
+++ prg51.py
@@ -0,0 +1,37 @@
+xy=int(input())
+
+s=[]
+
+rev1=0
+
+count=0
+
+while xy>0:
+
+    temp=xy%10
+
+    rev1=rev1*10+temp
+
+    xy=xy//10
+
+    count+=1
+
+while rev1>0:
+
+    temp=rev1%10
+
+    s.append(temp)
+
+    rev1=rev1//10
+
+for i in range(0,count):
+
+    if i<count-1:
+
+        g=' '
+
+    else:
+
+        g=''
+
+    print(s[i],end=g)
